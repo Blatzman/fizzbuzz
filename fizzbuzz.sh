961d310adc29f363fb0458b7e6a0fed936c4b166
Blatzman <Innerky@gmail.com>
2025-05-15 19:48:54 -0500

    fuck

---
+++ fizzbuzz.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+# Iterate through numbers 1 to 100
+ for ((i=1; i<=100; i++)); do
+	 #check if the number is divisible by 3 or 5
+	 if (( i % 3 == 0 )) || (( i % 5 == 0 )); then
+		 # Assign a name based on divisibility
+		 if (( i % 3 == 0 )) && (( i % 5 == 0 )); then
+			 name="fizzbutt"
+		 elif (( i % 3 == 0 )); then
+			 name="fizz"
+		 else
+			 name="butt"
+		 fi
+		 # Print the number and its assigned name
+		  echo "$i: $name"
+	 else
+		 # Print the number if not divisible by 3 or 5
+		 echo "$i"
+	fi
+done
+	
